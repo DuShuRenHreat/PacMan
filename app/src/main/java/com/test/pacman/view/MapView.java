1d2d2ce17ea63a0905bfbb6ccd7aa12551016588
DuShuRenHreat <abz514530236@163.com>
2017-04-20 21:17:51 +0800

    1

---
+++ app/src/main/java/com/test/pacman/view/MapView.java
@@ -0,0 +1,8 @@
+package com.test.pacman.view;
+
+/**
+ * Created by asdf on 2017/4/20.
+ */
+
+public class MapView {
+}
---
+++ app/src/main/java/com/test/pacman/view/PersonView.java
@@ -0,0 +1,20 @@
+package com.test.pacman.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by asdf on 2017/4/20.
+ */
+
+public class PersonView extends View{
+    public PersonView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
